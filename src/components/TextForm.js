b0951ae5e619638d1c0f147eedb1d0f1681149ff
@Dev_murli <42312132+murli1998@users.noreply.github.com>
2024-02-10 15:17:09 +0530

    ....

---
+++ src/components/TextForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("My new text is here......");
+  //  setText("god morning"); correct way to change text
+  const handleUpClick = () => {
+    console.log("Uppercase button was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLwClick = () => {
+    console.log("Lowercase button was clickled");
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    console.log("handle on change");
+    setText(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+          <button className="btn btn-secondary m-3" onClick={handleUpClick}>
+            Covert to Uppercase
+          </button>
+          <button className="btn btn-success m-3" onClick={handleLwClick}>
+            Covert to Lowercase
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        <h1>Your Text Summary : </h1>
+        <p>
+          {text.split(" ").length} Words and {text.length} Characters
+        </p>
+        <p>
+          Time to read : {0.008 * text.split(" ").length.toFixed(3)} minutes.
+        </p>
+
+        <h2>Text Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
+
+TextForm.defaultProps = {
+  heading: "This is heading",
+};
