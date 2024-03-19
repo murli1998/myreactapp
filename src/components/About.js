721c078dee91924b2f83ea17168911885ad411b1
@Dev_murli <42312132+murli1998@users.noreply.github.com>
2024-03-19 17:18:11 +0530

    adding content in about page

--- src/components/About.js
+++ src/components/About.js
@@ -78,7 +78,7 @@ export default function About(props) {
                 backgroundColor: props.mode === "dark" ? "#262626" : "#f5f4f2",
               }}
             >
-              Accordion Item #1
+              Your Comprehensive Text Utility Hub
             </button>
           </h2>
           <div
@@ -93,14 +93,11 @@ export default function About(props) {
                 backgroundColor: props.mode === "dark" ? "#454443" : "white",
               }}
             >
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              Unlock the full potential of your text with Textify – the ultimate
+              destination for all your text manipulation needs. Whether you're a
+              seasoned writer, a student working on an assignment, or simply
+              looking to tidy up your documents, Textify offers an array of
+              powerful tools to streamline your workflow.
             </div>
           </div>
         </div>
@@ -119,7 +116,7 @@ export default function About(props) {
                 backgroundColor: props.mode === "dark" ? "#262626" : "#f5f4f2",
               }}
             >
-              Accordion Item #2
+              Our Features:
             </button>
           </h2>
           <div
@@ -134,17 +131,44 @@ export default function About(props) {
                 backgroundColor: props.mode === "dark" ? "#454443" : "white",
               }}
             >
-              <strong>This is the second item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              <strong>Convert to Uppercase & Lowercase:</strong> Instantly
+              transform text to uppercase or lowercase with just a click. Say
+              goodbye to manual retyping! <br />
+              <br />
+              <strong>Remove Extra Spaces:</strong> Clean up messy text by
+              eliminating unnecessary spaces, ensuring your content looks
+              polished and professional.
+              <br />
+              <br />
+              <strong>Copy Text:</strong> Effortlessly copy text to your
+              clipboard with our convenient copy feature, making it easy to
+              transfer content wherever you need it.
+              <br />
+              <br />
+              <strong>Listen to Your Text:</strong> Sit back and listen as
+              Textify reads your text aloud, providing a convenient way to
+              proofread or absorb content hands-free.
+              <br />
+              <br />
+              <strong>Word Count & Character Count:</strong> Get precise word
+              and character counts at your fingertips, helping you stay on track
+              with document length requirements.
+              <br />
+              <br />
+              <strong>Time to Read:</strong> Estimate the time it takes to read
+              your text, perfect for presentations, speeches, or optimizing
+              content for readability.
+              <br />
+              <br />
+              <strong>More Features Coming Soon:</strong> We're constantly
+              evolving to meet your needs. Stay tuned for exciting updates and
+              new features on the horizon!
+              <br />
+              <br />
             </div>
           </div>
         </div>
+
         <div className="accordion-item">
           <h2 className="accordion-header">
             <button
@@ -160,7 +184,7 @@ export default function About(props) {
                 backgroundColor: props.mode === "dark" ? "#262626" : "#f5f4f2",
               }}
             >
-              Accordion Item #3
+              Why Choose Textify:
             </button>
           </h2>
           <div
@@ -175,14 +199,104 @@ export default function About(props) {
                 backgroundColor: props.mode === "dark" ? "#454443" : "white",
               }}
             >
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              <strong>Free to Use:</strong> Enjoy all our features at no cost.
+              Textify is committed to providing accessible tools for everyone.
+              <br />
+              <br />
+              <strong>Device Size Independent: </strong> Whether you're on a
+              desktop, laptop, tablet, or smartphone, Textify adapts seamlessly
+              to any screen size for a consistent user experience.
+              <br />
+              <br />
+              <strong>Fast and Reliable:</strong> Experience lightning-fast
+              performance and reliable results, ensuring efficiency in your text
+              processing tasks.
+              <br />
+              <br />
+              <strong>Privacy Assurance:</strong> Your privacy matters to us.
+              Textify prioritizes the security and confidentiality of your data.
+              <br />
+              <br />
+              <strong>All Browsers Compatible:</strong> Access Textify from your
+              preferred web browser with ease. Our platform is optimized for
+              compatibility across all major browsers.
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFour"
+              aria-expanded="false"
+              aria-controls="collapseFour"
+              // style={myStyle}
+              style={{
+                color: props.mode === "dark" ? "white" : "black",
+                backgroundColor: props.mode === "dark" ? "#262626" : "#f5f4f2",
+              }}
+            >
+              How to Get Started:
+            </button>
+          </h2>
+          <div
+            id="collapseFour"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div
+              className="accordion-body"
+              style={{
+                color: props.mode === "dark" ? "white" : "black",
+                backgroundColor: props.mode === "dark" ? "#454443" : "white",
+              }}
+            >
+              Using Textify is simple and intuitive. Just visit our website,
+              choose the tool you need, input your text, and let our powerful
+              features take care of the rest. No downloads, no
+              installations—just efficient text processing at your fingertips.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseFive"
+              aria-expanded="false"
+              aria-controls="collapseFive"
+              // style={myStyle}
+              style={{
+                color: props.mode === "dark" ? "white" : "black",
+                backgroundColor: props.mode === "dark" ? "#262626" : "#f5f4f2",
+              }}
+            >
+              Join the Textify Community Today:
+            </button>
+          </h2>
+          <div
+            id="collapseFive"
+            className="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div
+              className="accordion-body"
+              style={{
+                color: props.mode === "dark" ? "white" : "black",
+                backgroundColor: props.mode === "dark" ? "#454443" : "white",
+              }}
+            >
+              Thousands of users trust Textify for their text transformation
+              needs. Join our growing community of writers, students, and
+              professionals who rely on our tools to streamline their workflow
+              and achieve their goals. Experience the convenience of text
+              processing done right with Textify.
             </div>
           </div>
         </div>
